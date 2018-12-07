2e70e3610eb158f39663a88469977c588ef18beb
Nmircheski <nmircheski@gmail.com>
2018-12-07 18:40:04 +0100

    adding maven modules

--- zuul-api-gateway/src/main/java/com/microservices/transactions/zuulapigateway/ZuulApiGatewayApplication.java
+++ zuul-api-gateway/src/main/java/com/microservices/transactions/zuulapigateway/ZuulApiGatewayApplication.java
@@ -1,4 +1,5 @@
-package com.microservices.goji.zuulapigateway;
+package com.microservices.transactions.zuulapigateway;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
